a4047f9ab5552249d0f009eaaf1db751e330cc6a
Richenda Gould <Richenda Gould>
2015-11-11 21:16:10 -0500

    Part 3 dDDone

---
+++ js/concatenate.js
@@ -0,0 +1,8 @@
+function concatenater() {
+  var myName = document.getElementById("myName").value;
+  var myShow = document.getElementById("myShow").value;
+  var weekDay = document.getElementById("weekDay").value;
+
+  alert(myName + " watches " + myShow + " on " + weekDay + "s!");
+
+}
