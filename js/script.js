a509516b372484143f9c79fb9fbb8f546a2446a2
Richenda Gould <Richenda Gould>
2015-11-11 20:27:37 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function multiply() {
+  var firstNumber = document.getElementById("firstNumber").value;
+  var secondNumber = document.getElementById("secondNumber").value;
+ 
+ alert(firstNumber * secondNumber);
+}
